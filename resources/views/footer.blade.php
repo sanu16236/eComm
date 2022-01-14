44ae160b0ae443fc67e9464be43e0f2a16ced188
sanu16236 <sanu16236@gmail.com>
2022-01-14 17:20:00 +0530

    login module complete

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="text-center py-3">
+    <p>copyright &copy; 2022 | All Right Reserved</p>
+</div>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-comm project</title>
+
+    {{-- bootstrap cdn --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    {{ View::make('header') }}
+
+    @yield('content')
+
+    {{ View::make('footer') }}
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+</style>
+</html>
