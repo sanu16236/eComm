8e8854c64b5fca38c59a6825897047401c2206de
sanu16236 <sanu16236@gmail.com>
2022-01-14 19:49:59 +0530

    login completed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/login',[UserController::class,'login']);
+
+Route::get('/',[ProductController::class,'index']);
